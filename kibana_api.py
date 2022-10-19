458d0d22177c81a168dd5e6b80ef6a3662c4181f
Andrew Gizas <andreas.gkizas@elastic.co>
2022-10-19 17:28:52 +0300

    Update kibana_api.py

--- kibana_api.py
+++ kibana_api.py
@@ -81,5 +81,5 @@ api_keys_values=x.json()
 for list in api_keys_values['list']:
     if list['policy_id']=="agent-policy-id-automated":
         if list['active'] is True:
-            with open('finish.txt', 'wb') as fh:
+            with open('finish.txt', 'w') as fh:
               fh.write(list['api_key'])
